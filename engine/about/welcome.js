140f47c86c921508e09f3a13c2ece79e48a059eb
CanadaHonk <oj@oojmed.com>
2023-10-29 18:41:08 +0000

    welcome: tweaks, update js section

--- engine/about/welcome.js
+++ engine/about/welcome.js
@@ -39,13 +39,13 @@ export default () => `<title>Shadow</title>
 <p>here's a twist: <u>you're using it right now</u>! you can use the fps counter in the top right as an indicator. also, expect nothing to work :)</p>
 <p>here are some debug keybinds for you:</p>
 <ul>
-<li><b>z</b>: hold for inspect mode (hover over stuff)</li>
-<li><b>v</b>: prompt to load url</li>
-<li><b>r</b>: reload the current page</li>
-<li><b>h</b>: go back to welcome page (here)</li>
-<li><b>x</b>: switch color scheme (light/dark)</li>
-<li><b>c</b>: dump parsed html</li>
-<li><b>j</b>: cycle JS engine (host -> none -> SpiderMonkey -> Kiesel) (reloads the page)</li>
+<li><b>Z</b>: hold for inspect mode (hover over stuff)</li>
+<li><b>V</b>: prompt to load url</li>
+<li><b>R</b>: reload the current page</li>
+<li><b>H</b>: go back to welcome page (here)</li>
+<li><b>X</b>: switch color scheme (light/dark)</li>
+<li><b>C</b>: dump parsed html</li>
+<li><b>J</b>: cycle JS engine (host -> none -> SpiderMonkey -> Kiesel) (reloads the page)</li>
 </ul>
 <h2>demo sites</h2>
 <ul>${demos.map(x => `<li><a href="${x[0]}">${x[0]}</a> (${x[1]})</li>`).join('\n')}</ul>
@@ -59,12 +59,14 @@ export default () => `<title>Shadow</title>
 </ul>
 
 <h2>javascript <span class="new">new!</span></h2>
-<p>Shadow has extremely experimental javascript support. this is not intended to be usable, rather a proof of concept. off by default.</p>
-<p>bonus: <b>you can choose which JS engine to use!</b></p>
+<p>Shadow has some experimental javascript support, with a very limited DOM api (<code>document</code>, <code>window</code>, etc).</p>
+<p>bonus: <b>you can choose which JS engine to use!</b> (press J to cycle between)</p>
 
 <ul>
-<li><a href="https://spidermonkey.dev" target="_parent">SpiderMonkey</a> (Firefox's JS engine)</li>
-<li><a href="https://kiesel.dev" target="_parent">Kiesel</a> (a WIP engine from scratch in Zig)</li>
+<li>host (your browser's) - default</li>
+<li>none (no JS)</li>
+<li><a href="https://spidermonkey.dev" target="_parent">SpiderMonkey</a> (Firefox's JS engine, Wasm)</li>
+<li><a href="https://kiesel.dev" target="_parent">Kiesel</a> (a WIP engine from scratch in Zig, Wasm)</li>
 </ul>
 
 <button onclick="let el = document.querySelector('#counter'); el.textContent = parseInt(el.textContent) + 1">click me!</button>&nbsp;<span id="counter">0</span><noscript dynamic=true>(you have JS disabled, press J)</noscript>
