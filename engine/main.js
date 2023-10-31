9d4284db4bd9ba7754f10a71b3aa71b4a63fb459
CanadaHonk <oj@oojmed.com>
2023-10-31 02:50:49 +0000

    version: 2023.10.30

--- engine/main.js
+++ engine/main.js
@@ -13,7 +13,7 @@ if (window.crossOriginIsolated === false) {
   // if (worker.active.state === 'activated') location.reload();
 }
 
-window.version = `2023.10.29`;
+window.version = `2023.10.30`;
 
 const welcome = () => load('about:welcome');
 window.welcome = welcome;
